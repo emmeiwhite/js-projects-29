c2a3cbc2df1cd554a2068c0484e9318c057efd31
Imran Rafiq Rather <emmeiwhite@gmail.com>
2023-07-09 18:56:23 +0530

    color generator

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,9 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025", "yellow"];
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", function (e) {
+  const currentIndex = Math.floor(Math.random() * colors.length);
+  console.log(currentIndex);
+  document.body.style.backgroundColor = colors[currentIndex];
+});
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,23 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+// Idea is to create hex code as the user clicks. So, we need to have combination of 6 digits from 0-f. Loop will run 6 times and we will keep adding digit each time until our hex code is ready
+
+const button = document.getElementById("btn");
+const textDiv = document.querySelector(".color");
+let color = "#";
+const getColor = function () {
+  for (let i = 0; i < 6; i++) {
+    // Select random number from the array
+
+    const randomValue = Math.floor(Math.random() * hex.length); // [0, 15)
+    color += hex[randomValue];
+  }
+  console.log(color);
+};
+
+button.addEventListener("click", () => {
+  getColor();
+  document.body.style.backgroundColor = color;
+  textDiv.textContent = color;
+  color = "#";
+});
