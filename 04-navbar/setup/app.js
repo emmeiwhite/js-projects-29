99f124f3bf88bcc47d7a697fb4d76c69008ee74f
Imran Rafiq Rather <emmeiwhite@gmail.com>
2023-08-19 22:44:58 +0530

    Navbar done. More focus on js than css

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -8,14 +8,5 @@ const toggleBtn = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 
 toggleBtn.addEventListener("click", function () {
-  /*
-    console.log(links.classList);
-  if (links.classList.contains("show-links")) {
-    links.classList.remove("show-links");
-  } else {
-    links.classList.add("show-links");
-  }
-  */
-
   links.classList.toggle("show-links");
 });
