a406fbfe7e33d3af26211ed61fed9123bfd9fec2
Imran Rafiq Rather <emmeiwhite@gmail.com>
2023-08-20 00:17:09 +0530

    Model done

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -2,3 +2,22 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+const modelContainer = document.querySelector(".model-container");
+const showButton = document.querySelector(".show-model");
+const closeButton = document.querySelector(".close-btn");
+
+showButton.addEventListener("click", function () {
+  modelContainer.classList.add("show");
+});
+
+closeButton.addEventListener("click", function () {
+  modelContainer.classList.remove("show");
+});
+
+modelContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("model-content")) {
+    return;
+  }
+  modelContainer.classList.remove("show");
+});
