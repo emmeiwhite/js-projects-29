39d5646c526102ab5a8ab18232e287d128fa9a2a
Imran Rafiq Rather <emmeiwhite@gmail.com>
2023-11-03 12:39:04 +0530

    Numbers Count Project was awesome"

--- 18-numbers/starter/app.js
+++ 18-numbers/starter/app.js
@@ -1,1 +1,24 @@
-console.log('numbers project');
+const items = [...document.querySelectorAll(".number")];
+console.log(items);
+
+// Iterating over each Array Element
+items.forEach((item) => {
+  updateCount(item);
+});
+
+function updateCount(el) {
+  const value = parseInt(el.dataset.value);
+  const increment = Math.ceil(value / 1000);
+  let initialValue = 0;
+
+  const increaseCount = setInterval(() => {
+    initialValue += increment;
+
+    if (initialValue > value) {
+      el.textContent = `${value}+`;
+      clearInterval(increaseCount);
+      return;
+    }
+    el.textContent = `${initialValue}+`;
+  }, 1);
+}
